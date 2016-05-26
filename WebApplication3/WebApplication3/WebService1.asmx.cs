f8d2d488f1146828cb37af954a755ecfa3ab94e6
laiquesantana <laiquesantana@hotmail.com>
2016-05-25 23:41:28 -0300

    verificar acentos

---
+++ WebApplication3/WebApplication3/WebService1.asmx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+
+namespace WebApplication3
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+
+
+    public  class Suporte
+    {
+        public static string RemoveAcentos(string texto)
+        {
+            if (texto == null) return string.Empty;
+
+            string comAcentos = "ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇç";
+            string semAcentos = "AAAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUuuuuCc";
+
+            for (int i = 0; i < comAcentos.Length; i++)
+                texto = texto.Replace(comAcentos[i].ToString(), semAcentos[i].ToString());
+
+            return texto;
+        }
+    }
+
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        string suporte;
+        Suporte p1 = new Suporte();
+
+
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            suporte = Suporte.RemoveAcentos("ciêntifica e matématica lógica Vô");
+            return suporte;
+        }
+
+
+    }
+
+
+   
+
+}
